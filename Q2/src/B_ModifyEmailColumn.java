b9bffc03d88b963a03df326d166d17b359bfcc9f
Karthik-Mahadev <139464228+Karthik-Mahadev@users.noreply.github.com>
2023-09-09 22:26:24 +0530

    Added files

---
+++ Q2/src/B_ModifyEmailColumn.java
@@ -0,0 +1,23 @@
+package Q2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class B_ModifyEmailColumn {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/GL";
+        String username = "root";
+        String password = "Live@1976";
+
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             Statement stmt = conn.createStatement()) {
+            String sql = "ALTER TABLE employee MODIFY Email_Id VARCHAR(30) NOT NULL";
+            stmt.executeUpdate(sql);
+            System.out.println("Email_Id column modified successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
